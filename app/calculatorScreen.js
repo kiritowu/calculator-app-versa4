b06db36cddc313002b2432d9a17356b322c4ed9d
kiritowu <wongzhaowu@gmail.com>
2024-08-11 19:17:15 +0800

    Use Ionic Views framework for existing calculator app

---
+++ app/calculatorScreen.js
@@ -0,0 +1,103 @@
+import { View, $at } from './view'
+import { id2Symbol, opsMethods } from "./enums";
+
+// Create the root selector for the view...
+const $ = $at('#calculator-screen');
+
+export class CalculatorScreen extends View {
+    // Specify the root view element.
+    // When set, it will be used to show/hide the view on mount and unmount.
+    // Root view element used to show/hide the view.
+    el = $(); // Extract #calculator-screen element.
+
+    // Ad-hoc $-queries must be avoided.
+    // You've got dumb 120MHz MCU with no JIT in VM, thus everything you do is expensive.
+    // Put all of your elements here, like this:
+
+    // otherEl = $( '#other-el-id' );
+    numberConsole = $('#numbers');
+    stage = undefined;
+
+    // Lifecycle hook executed on `view.mount()`.
+    onMount() {
+        // Mount event listeners
+        Object.keys(id2Symbol).forEach(id => {
+            // TODO: vibrate and change opacity on mousedown
+            $(`#${id}`).addEventListener("click", this._appendSymbolHandler);
+        })
+        $("#equal").addEventListener("click", this._equalHandler);
+        $("#back").addEventListener("click", this._backspaceHandler);
+        $("#clear").addEventListener("click", this._clearHandler);
+    }
+
+    // Lifecycle hook executed on `view.unmount()`.
+    onUnmount() {
+        // Unmount local props
+        this.stage = null;
+        this.numberConsole.text = "";
+
+        // Unmount event listeners
+        Object.keys(id2Symbol).forEach(id => {
+            // TODO: vibrate and change opacity on mousedown
+            $(`#${id}`).removeEventListener("click", this._appendSymbolHandler);
+        })
+        $("#equal").removeEventListener("click", this._equalHandler);
+        $("#back").removeEventListener("click", this._backspaceHandler);
+        $("#clear").removeEventListener("click", this._clearHandler);
+    }
+
+    // Custom UI update logic, executed on `view.render()`.
+    onRender() {
+        // TODO: put DOM manipulations here...
+        // Call this.render() to update UI.
+    }
+
+    _resolveOps(rightNum, stage) {
+        // Recursion to evaluate equation
+        if (stage === undefined) {
+            return rightNum;
+        }
+        const { num, ops, nextStage } = stage;
+        const leftNum = resolveOps(num, nextStage);
+        return ops(leftNum, rightNum);
+    }
+
+    _appendSymbolHandler(id) {
+        console.log("appending " + id);
+        const handler = (event) => {
+            for (let opsMethod of Object.keys(opsMethods)) {
+                // Fallback to use for of since includes is not available
+                if (id === opsMethod) {
+                    // Stage numeric operation
+                    this.stage = { num: parseFloat(this.numberConsole.text), ops: opsMethods[id], nextStage: this.stage };
+                    this.numberConsole.text = id2Symbol[id];
+                    return;
+                }
+            }
+            // Append number symbol
+            this.numberConsole.text += id2Symbol[id];
+        }
+        return handler
+    }
+
+
+    _equalHandler(event) {
+        console.log("click equal");
+        const result = this._resolveOps(parsefloat(this.numberConsole.text.slice(1)), this.stage);
+        this.numberConsole.text = result % 1 === 0 ? result : result.toprecision(8);
+        this.stage = undefined;
+    }
+
+
+    _backspaceHandler(event) {
+        // Remove last character in numberConsole
+        this.numberConsole.text = this.numberConsole.text.slice(0, -1);
+    }
+
+    _clearHandler(event) {
+        // Clear number console and stage
+        this.numberConsole.text = "";
+        this.stage = undefined;
+    }
+
+}
--- app/index.js
+++ app/index.js
@@ -1,85 +1,17 @@
-/*
- * Entry point for the watch app
- */
-import * as document from "document";
+import document from "document";
+import { Application } from './view'
+import { CalculatorScreen } from './calculatorScreen'
 
-import { id2Symbol, opsMethods } from "./enums";
+class MultiScreenApp extends Application {
+    // List all screens
+    screens = { CalculatorScreen }
 
-function main() {
-    const numberConsole = document.getElementById("numbers");
-    let stage = undefined;
-
-    function appendSymbol(id) {
-        console.log("appending " + id);
-        for (let opsMethod of Object.keys(opsMethods)) {
-            // Fallback to use for of since includes is not available
-            if (id === opsMethod) {
-                // if (Object.keys(symbol2Id).includes(numberConsole.text) && stage !== undefined) {
-                //     // Replace operator
-                //     stage = {
-                //         num: stage.num,
-                //         ops: opsMethods[id],
-                //         nextStage: stage.nextStage
-                //     }
-                //     return;
-                // }
-                // Stage operator
-                stage = { num: parseFloat(numberConsole.text), ops: opsMethods[id], nextStage: stage };
-                numberConsole.text = id2Symbol[id];
-                return;
-            }
-        }
-        // Append number symbol
-        numberConsole.text += id2Symbol[id];
+    // "down" key handler
+    onKeyDown() {
+        // Switch between two screens we have.
+        // Application.switchTo(this.screen.constructor === Screen1 ? 'Screen2' : 'Screen1');
     }
-
-
-    function resolveOps(rightNum, stage) {
-        // Recursion to eval ops
-        console.log(rightNum)
-        if (stage === undefined) {
-            return rightNum;
-        }
-        const { num, ops, nextStage } = stage;
-        const leftNum = resolveOps(num, nextStage);
-        return ops(leftNum, rightNum);
-    }
-
-
-    function backspace() {
-        numberConsole.text = numberConsole.text.slice(0, -1);
-    }
-
-    function flush() {
-        numberConsole.text = "";
-        stage = undefined;
-    }
-
-    Object.keys(id2Symbol).forEach(id => {
-        // TODO: vibrate and change opacity on mousedown
-        document.getElementById(id).addEventListener("click", (evt) => {
-            // TODO: remove debug statement
-            console.log(`click ${id}`);
-            appendSymbol(id);
-        });
-    })
-
-    document.getElementById("equal").addEventListener("click", (evt) => {
-        console.log("click equal");
-        const result = resolveOps(parseFloat(numberConsole.text.slice(1)), stage);
-        numberConsole.text = result % 1 === 0 ? result : result.toPrecision(8);
-        stage = undefined;
-    });
-
-    document.getElementById("back").addEventListener("click", (evt) => {
-        console.log("click back");
-        backspace();
-    });
-
-    document.getElementById("clear").addEventListener("click", (evt) => {
-        console.log("click clear");
-        flush();
-    });
 }
 
-main();
+// Start the application with Screen1.
+MultiScreenApp.start('CalculatorScreen');
